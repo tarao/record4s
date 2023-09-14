f4ef8cfc7aa03aa192fb6c322999fd4995c53e54
tarao <tarao.gnn@gmail.com>
2023-09-14 18:13:09 +0900

    Documentation and comments.

--- src/main/scala/com/github/tarao/record4s/Record.scala
+++ src/main/scala/com/github/tarao/record4s/Record.scala
@@ -1,21 +1,82 @@
 package com.github.tarao.record4s
 
+/** Base trait for record types.
+  *
+  * This trait is a placeholder to avoid trouble with defining methods on companion object
+  * of `%`.
+  */
 trait Record
 
 object Record {
+  /** An empty record. */
   val empty: % = new MapRecord(Map.empty)
 
   given canEqualReflexive[R <: %]: CanEqual[R, R] = CanEqual.derived
 
   extension [R <: %](record: R) {
+    /** Extends fields of the record.
+      *
+      * If a new field has the same name as the existing field, then the new field
+      * overrides the old one.
+      *
+      * @example {{{
+      * val r = %(name = "tarao") + (age = 3, email = "tarao@example.com")
+      * // val r: com.github.tarao.record4s.%{val name: String; val age: Int; val email: String} = %(name = tarao, age = 3, email = tarao@example.com)
+      * }}}
+      *
+      * @return an object to define new fields
+      */
     def + : Extensible[R] = new Extensible(record)
 
-    transparent inline def ++[R2](other: R2) =
+    /** Concatenate this record and another record.
+      *
+      * If the both record has a field of the same name, then it takes the field from the
+      * latter record.
+      *
+      * @example {{{
+      * val r1 = %(name = "tarao", age = 3)
+      * val r2 = %(age = 4, email = "tarao@example.com")
+      * val r3 = r1 ++ r2
+      * // val r3: com.github.tarao.record4s.%{val name: String; val age: Int; val email: String} = %(name = tarao, age = 4, email = tarao@example.com)
+      * }}}
+      *
+      * @param   R2     a record type (given `RecordLike[R2]`)
+      * @param   other  a record to concatenate
+      * @return  a new record which has the both fields from this record and `other`
+      */
+    transparent inline def ++[R2](other: R2)(using RecordLike[R2]) =
       ${ Macros.concatImpl('record, 'other) }
 
-    inline def |+|[R2 <: %](other: R2): R & R2 =
+    /** Concatenate this record and another record.
+      *
+      * A field of the same name in the both record is statically rejected.
+      *
+      * @example {{{
+      * val r1 = %(name = "tarao", age = 3)
+      * val r2 = %(email = "tarao@example.com")
+      * val r3 = r1 |+| r2
+      * // val r3: com.github.tarao.record4s.%{val name: String; val age: Int} & com.github.tarao.record4s.%{val email: String} = %(name = tarao, age = 3, email = tarao@example.com)
+      * }}}
+      *
+      * @param   R2     a record type (given `RecordLike[R2]`)
+      * @param   other  a record to concatenate
+      * @return  a new record which has the both fields from this record and `other`
+      */
+    inline def |+|[R2 <: %](other: R2)(using RecordLike[R2]): R & R2 =
       ${ Macros.concatDirectlyImpl('record, 'other) }
 
+    /** Upcast the record to specified type.
+      *
+      * @example {{{
+      * val r1 = %(name = "tarao", age = 3, email = "tarao@example.com")
+      * // val r1: com.github.tarao.record4s.%{val name: String; val age: Int; val email: String} = %(name = tarao, age = 3, email = tarao@example.com)
+      * val r2 = r1.as[% { val name: String; val age: Int }]
+      * // val r2: com.github.tarao.record4s.%{val name: String; val age: Int} = %(name = tarao, age = 3)
+      * }}}
+      *
+      * @param R2  target type
+      * @return    a record containing only fields in the target type
+      */
     inline def as[R2 >: R]: R2 =
       ${ Macros.upcastImpl[R, R2]('record) }
   }
@@ -27,6 +88,17 @@ object Record {
   }
 }
 
+/** Base class for records.
+  *
+  * Example {{{
+  * val r = %(name = "tarao", age = 3)
+  * // val r: com.github.tarao.record4s.%{val name: String; val age: Int} = %(name = tarao, age = 3)
+  * r.name
+  * // val res0: String = tarao
+  * r.age
+  * // val res1: Int = 3
+  * }}}
+  */
 abstract class % extends Record with Selectable {
   private[record4s] def __data: Map[String, Any]
 
@@ -46,4 +118,8 @@ abstract class % extends Record with Selectable {
 
 val % = new Extensible(Record.empty)
 
+/** A concrete record class.
+  *
+  * This class is exposed due to inlining but not intended to be used directly.
+  */
 final class MapRecord(private[record4s] val __data: Map[String, Any]) extends %
