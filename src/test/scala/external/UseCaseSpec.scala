12455be2d5cf56e85b8be1bc5cbeeba55ed23ff6
tarao <tarao.gnn@gmail.com>
2023-09-22 14:15:17 +0900

    Add some tests.

--- src/test/scala/external/UseCaseSpec.scala
+++ src/test/scala/external/UseCaseSpec.scala
@@ -2,7 +2,7 @@ package external
 
 class UseCaseSpec extends helper.UnitSpec {
   describe("Generic record extension with ++") {
-    import com.github.tarao.record4s.%
+    import com.github.tarao.record4s.{%, Tag}
     import com.github.tarao.record4s.typing.Concat
 
     it("can be done by using Concat") {
@@ -49,10 +49,62 @@ class UseCaseSpec extends helper.UnitSpec {
       r1.email.user shouldBe "tarao"
       r1.email.domain shouldBe "example.com"
     }
+
+    it("preserves a tag") {
+      trait Person
+      object Person {
+        extension [R <: % { val name: String }](p: R & Tag[Person]) {
+          def firstName: String = p.name.split(" ").head
+
+          def withEmail[RR <: %](email: String)(using
+            Concat.Aux[R & Tag[Person], % { val email: String }, RR],
+          ): RR = p ++ %(email = email)
+        }
+      }
+
+      def addEmail[R <: %, RR <: %](record: R, email: String)(using
+        Concat.Aux[R, % { val email: String }, RR],
+      ): RR = record ++ %(email = email)
+
+      val r0 = %(name = "tarao fuguta", age = 3).tag[Person]
+      val r1 = addEmail(r0, "tarao@example.com")
+      r1 shouldBe a[% { val name: String; val age: Int; val email: String }]
+      r1.name shouldBe "tarao fuguta"
+      r1.firstName shouldBe "tarao"
+      r1.age shouldBe 3
+      r1.email shouldBe "tarao@example.com"
+
+      val r2 = r0.withEmail("tarao@example.com")
+      r2 shouldBe a[% { val name: String; val age: Int; val email: String }]
+      r2.name shouldBe "tarao fuguta"
+      r2.firstName shouldBe "tarao"
+      r2.age shouldBe 3
+      r2.email shouldBe "tarao@example.com"
+    }
+
+    it("should lose record type information without using Concat") {
+      def addEmail[R <: %](record: R, email: String) =
+        record ++ %(email = email)
+
+      val r0 = %(name = "tarao", age = 3)
+      val r1 = addEmail(r0, "tarao@example.com")
+      "r1.name" shouldNot compile
+      "r1.age" shouldNot compile
+      r1.email shouldBe "tarao@example.com"
+      r1 shouldBe a[% { val email: String }]
+
+      def addEmail2[R <: % { val name: String }](record: R, email: String) =
+        record ++ %(email = email)
+      val r2 = addEmail(r0, "tarao@example.com")
+      "r2.name" shouldNot compile
+      "r2.age" shouldNot compile
+      r2.email shouldBe "tarao@example.com"
+      r2 shouldBe a[% { val email: String }]
+    }
   }
 
   describe("Generic record extension with +") {
-    import com.github.tarao.record4s.%
+    import com.github.tarao.record4s.{%, Tag}
     import com.github.tarao.record4s.typing.Append
 
     it("can be done by using Append") {
@@ -98,6 +150,58 @@ class UseCaseSpec extends helper.UnitSpec {
       r1.age shouldBe 3
       r1.email.user shouldBe "tarao"
       r1.email.domain shouldBe "example.com"
+    }
+
+    it("preserves a tag") {
+      trait Person
+      object Person {
+        extension [R <: % { val name: String }](p: R & Tag[Person]) {
+          def firstName: String = p.name.split(" ").head
+
+          def withEmail[RR <: %](email: String)(using
+            Append.Aux[R & Tag[Person], ("email", String) *: EmptyTuple, RR],
+          ): RR = p + (email = email)
+        }
+      }
+
+      def addEmail[R <: %, RR <: %](record: R, email: String)(using
+        Append.Aux[R, ("email", String) *: EmptyTuple, RR],
+      ): RR = record + (email = email)
+
+      val r0 = %(name = "tarao fuguta", age = 3).tag[Person]
+      val r1 = addEmail(r0, "tarao@example.com")
+      r1 shouldBe a[% { val name: String; val age: Int; val email: String }]
+      r1.name shouldBe "tarao fuguta"
+      r1.firstName shouldBe "tarao"
+      r1.age shouldBe 3
+      r1.email shouldBe "tarao@example.com"
+
+      val r2 = r0.withEmail("tarao@example.com")
+      r2 shouldBe a[% { val name: String; val age: Int; val email: String }]
+      r2.name shouldBe "tarao fuguta"
+      r2.firstName shouldBe "tarao"
+      r2.age shouldBe 3
+      r2.email shouldBe "tarao@example.com"
+    }
+
+    it("should lose record type information without using Append") {
+      def addEmail[R <: %](record: R, email: String) =
+        record + (email = email)
+
+      val r0 = %(name = "tarao", age = 3)
+      val r1 = addEmail(r0, "tarao@example.com")
+      "r1.name" shouldNot compile
+      "r1.age" shouldNot compile
+      r1.email shouldBe "tarao@example.com"
+      r1 shouldBe a[% { val email: String }]
+
+      def addEmail2[R <: % { val name: String }](record: R, email: String) =
+        record + (email = email)
+      val r2 = addEmail(r0, "tarao@example.com")
+      "r2.name" shouldNot compile
+      "r2.age" shouldNot compile
+      r2.email shouldBe "tarao@example.com"
+      r2 shouldBe a[% { val email: String }]
     }
   }
 }
