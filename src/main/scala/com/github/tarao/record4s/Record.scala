0fb7f4337f2a5161fbd667d32c80f7423d4b458d
tarao <tarao.gnn@gmail.com>
2023-09-22 14:29:29 +0900

    Move Extensible class onto Record object.

--- src/main/scala/com/github/tarao/record4s/Record.scala
+++ src/main/scala/com/github/tarao/record4s/Record.scala
@@ -190,6 +190,20 @@ object Record {
 
   private def newMapRecord[R <: %](record: Iterable[(String, Any)]): R =
     new MapRecord(record.toMap).asInstanceOf[R]
+
+  import scala.language.dynamics
+
+  class Extensible[R](private val record: R) extends AnyVal with Dynamic {
+    transparent inline def applyDynamic(method: String)(
+      inline fields: (String, Any)*,
+    ) =
+      ${ Macros.applyImpl('record, 'method, 'fields) }
+
+    transparent inline def applyDynamicNamed(method: String)(
+      inline fields: (String, Any)*,
+    ) =
+      ${ Macros.applyImpl('record, 'method, 'fields) }
+  }
 }
 
 /** Base class for records.
@@ -222,7 +236,7 @@ abstract class % extends Record with Selectable {
     }
 }
 
-val % = new Extensible(Record.empty)
+val % = new Record.Extensible(Record.empty)
 
 /** A concrete record class.
   *
