d889358ce631166af8c3c3b3fef127cd5b74d505
tarao <tarao.gnn@gmail.com>
2023-10-09 00:47:12 +0900

    Accumulate outputs from targets.

--- script/benchmark.sh
+++ script/benchmark.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/env bash
 
 set -xe
 
@@ -8,15 +8,35 @@ cd "$ROOT"
 OUT_DIR="target/benchmark"
 OPTIONS="-wi 10 -i 20 -t 1 -f 10"
 
+sbt=sbt
+
+OUTPUTS=()
+
 run() {
     PROJECT=$1
     TARGET=$2
     FEATURE=$3
 
     mkdir -p "${OUT_DIR}/${TARGET}"
-    OUTPUT="../../${OUT_DIR}/${TARGET}/${FEATURE}.json"
+    OUTPUT="${OUT_DIR}/${TARGET}/${FEATURE}.json"
 
-    sbt "${PROJECT} / Jmh / run $OPTIONS ${TARGET}.${FEATURE} -rf json -rff \"${OUTPUT}\""
+    $sbt "${PROJECT} / Jmh / run $OPTIONS ${TARGET}.${FEATURE} -rf json -rff \"../../${OUTPUT}\""
+    OUTPUTS+=("$OUTPUT")
+}
+
+to_json_rows() {
+    jq -s '[ .[] | {
+        benchmark:.benchmark,
+        target: .benchmark | sub("^benchmark[.](?<t>[^.]+)[.].*$"; "\(.t)"),
+        feature: .benchmark | sub("^benchmark[.][^.]+[.](?<f>[^.]+)[.].*$"; "\(.f)"),
+        index: .benchmark | sub("^.*[^0-9](?<x>[0-9]+)$"; "\(.x)") | tonumber,
+        score: (.primaryMetric.rawData[] | map({value:.}))[] | .value
+    } ]'
+}
+
+[ "$1" = "-0" ] && { # dry run
+    shift
+    sbt=:
 }
 
 [ "$1" = "-1" ] && { # just run once for test
@@ -26,7 +46,22 @@ run() {
 
 [ -n "$1" ] && [ -n "$2" ] && [ -n "$3" ] && {
     run "$1" "$2" "$3"
+    CHART_INPUT="${OUT_DIR}/${FEATURE}_${TARGET}.json"
+    jq '.[]' "${OUTPUT}" | to_json_rows > "${CHART_INPUT}"
     exit
 }
 
-run "benchmark_3" "record4s" "FieldAccess"
+run_feature() {
+    FEATURE="$1"
+    run "benchmark_3" "record4s" "${FEATURE}"
+
+    CHART_INPUT="${OUT_DIR}/${FEATURE}.json"
+
+    for output in ${OUTPUTS[@]}; do
+        jq '.[]' "${output}"
+    done | to_json_rows > "${CHART_INPUT}"
+
+    OUTPUTS=()
+}
+
+run_feature "FieldAccess"
