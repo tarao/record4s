55a1faf0c6b094d62593c4a7568b8a7622661332
tarao <tarao.gnn@gmail.com>
2023-10-11 15:13:14 +0900

    Benchmark of `System.nanoTime()`.

---
+++ modules/benchmark_3/src/main/scala/benchmark/TimersBench.scala
@@ -0,0 +1,28 @@
+package benchmark
+
+import java.util.concurrent.TimeUnit;
+import org.openjdk.jmh.annotations.*
+
+@BenchmarkMode(Array(Mode.AverageTime))
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Warmup(iterations = 5, time = 100, timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(iterations = 5, time = 100, timeUnit = TimeUnit.MILLISECONDS)
+@Fork(5)
+@State(Scope.Thread)
+class TimersBench {
+  var lastValue: Long = 0
+
+  @Benchmark
+  def latencyNanotime(): Long = System.nanoTime()
+
+  @Benchmark
+  def granularityNanotime(): Long = {
+    var cur: Long = 0
+    while ({
+      cur = System.nanoTime()
+      cur == lastValue
+    })()
+    lastValue = cur
+    cur
+  }
+}
