e16a169551481cedc1afaae8a63f2434fd3fc50f
tarao <tarao.gnn@gmail.com>
2023-11-01 19:04:11 +0900

    Test typing error messages of ArrayRecord.

--- modules/core/src/test/scala/com/github/tarao/record4s/TypeErrorSpec.scala
+++ modules/core/src/test/scala/com/github/tarao/record4s/TypeErrorSpec.scala
@@ -3,7 +3,7 @@ package com.github.tarao.record4s
 import scala.compiletime.testing.{typeCheckErrors, Error, ErrorKind}
 
 class TypeErrorSpec extends helper.UnitSpec {
-  describe("Typing") {
+  describe("Typing %") {
     describe("Concat") {
       it("should detect invalid characters in field labels") {
         def checkErrors(errs: List[Error]): Unit = {
@@ -99,6 +99,108 @@ class TypeErrorSpec extends helper.UnitSpec {
       it("should detect non-literal label type") {
         val errs =
           typeCheckErrors("""%(name = "tarao") ++ (("age", 3) *: EmptyTuple)""")
+        errs should not be empty
+        errs.head.kind shouldBe ErrorKind.Typer
+        errs.head.message should startWith(
+          "Types of field labels must be literal string types",
+        )
+      }
+    }
+  }
+
+  describe("Typing ArrayRecord") {
+    describe("Concat") {
+      it("should detect invalid characters in field labels") {
+        def checkErrors(errs: List[Error]): Unit = {
+          errs should not be empty
+          errs.head.kind shouldBe ErrorKind.Typer
+          val _ = errs.head.message should startWith(
+            "'$' cannot be used in a field label",
+          )
+        }
+
+        case class Cell($value: Int)
+
+        checkErrors(typeCheckErrors("""ArrayRecord(name = "tarao") ++ Cell(3)"""))
+        checkErrors(typeCheckErrors("""ArrayRecord.from(Cell(3))"""))
+      }
+
+      it("should detect wrong usage") {
+        """
+          def addEmail[R <: %, RR <: %](record: ArrayRecord[R], email: String)(using
+            typing.Concat.Aux[R, Nothing, RR],
+          ): ArrayRecord[RR] = record ++ ArrayRecord(email = email)
+        """ shouldNot typeCheck
+
+        """
+          def addEmail[R <: %, RR <: %](record: ArrayRecord[R], email: String)(using
+            typing.Concat.Aux[R, % { val name: String }, RR],
+          ): ArrayRecord[RR] = record ++ ArrayRecord(email = email)
+        """ shouldNot typeCheck
+      }
+    }
+
+    describe("Lookup") {
+      it("should detect illegal member access") {
+        val r = ArrayRecord(name = "tarao")
+        val errs = typeCheckErrors("""ArrayRecord.lookup(r, "email")""")
+        errs should not be empty
+        errs.head.kind shouldBe ErrorKind.Typer
+        errs.head.message should startWith("Value 'email' is not a member of")
+      }
+
+      it("should detect invalid key type") {
+        val r = ArrayRecord(name = "tarao")
+        val key = "email"
+        val errs = typeCheckErrors("""ArrayRecord.lookup(r, key)""")
+        errs should not be empty
+        errs.head.kind shouldBe ErrorKind.Typer
+        errs.head.message should startWith("Field label must be a literal string")
+      }
+    }
+
+    describe("Select") {
+      it("should detect missing key") {
+        def checkErrors(errs: List[Error]): Unit = {
+          errs should not be empty
+          errs.head.kind shouldBe ErrorKind.Typer
+          val _ =
+            errs.head.message should include regex "Missing key \\\\?'age\\\\?'"
+        }
+
+        val r = ArrayRecord(name = "tarao")
+        checkErrors(typeCheckErrors("""r(select.age)"""))
+      }
+
+      it("should detect invalid selector type") {
+        val r = ArrayRecord(name = "tarao")
+        val key = "name"
+        val s = new Selector[key.type *: EmptyTuple]
+        val errs = typeCheckErrors("""r(s)""")
+        errs should not be empty
+        errs.head.kind shouldBe ErrorKind.Typer
+        errs.head.message should startWith(
+          "Selector type element must be a literal",
+        )
+      }
+    }
+
+    describe("Unselect") {
+      it("should detect invalid unselector type") {
+        val r = ArrayRecord(name = "tarao")
+        val key = "name"
+        val u = new Unselector[key.type *: EmptyTuple]
+        val errs = typeCheckErrors("""r(u)""")
+        errs should not be empty
+        errs.head.kind shouldBe ErrorKind.Typer
+        // the error message is about proving R <:< RR
+      }
+    }
+
+    describe("RecordLike") {
+      it("should detect non-literal label type") {
+        val errs =
+          typeCheckErrors("""ArrayRecord(name = "tarao") ++ (("age", 3) *: EmptyTuple)""")
         errs should not be empty
         errs.head.kind shouldBe ErrorKind.Typer
         errs.head.message should startWith(
